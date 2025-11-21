b7583b6c8e77f9ab6575c8a3a17dd9681361712a
Torbjörn Sjögren <torbjorn.sjogren@gmail.com>
2025-11-21 11:59:35 +0100

    allow-argument-mismatch

--- fortran/build_dns_fortran_min.sh
+++ fortran/build_dns_fortran_min.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 export FC=gfortran
+export FFLAGS="-Ofast -mcpu=native -funroll-loops -std=legacy \
+  -ffixed-line-length-none -fno-automatic -finit-local-zero \
+  -fallow-argument-mismatch"
 
-# Common flags for old F77 / fixed-form
-# NOTE: add -w to suppress *all* warnings so Meson can't turn them into errors.
-export FFLAGS_COMMON="-std=legacy -ffixed-line-length-none -Wno-tabs \
-  -fno-automatic -finit-local-zero -fallow-argument-mismatch -w"
-
-# Release flags, export FFLAGS="$FFLAGS_COMMON -O3"
-export FFLAGS="$FFLAGS_COMMON -Ofast -mcpu=native -funroll-loops -flto"
+# If you still want to completely silence warnings, you can optionally add:
+#  -w -Wno-tabs
+# to FFLAGS, e.g.:
+# export FFLAGS="$FFLAGS -w -Wno-tabs"
 
 rm -f dns_fortran.cpython-313-darwin.so
 
