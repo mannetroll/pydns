0a6130ad38d058d0567850c89aa374ae9977bf7f
Torbjörn Sjögren <torbjorn.sjogren@gmail.com>
2025-11-23 18:11:18 +0100

    fortran_test

--- fortran/fortran_test.sh
+++ fortran/fortran_test.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-rm dns_test.exe
+rm fortran_test.exe
 rm *.o
 
 FFLAGS_COMMON="-std=legacy -ffixed-line-length-none -Wno-tabs -fno-automatic -finit-local-zero -fallow-argument-mismatch"
 FFLAGS="$FFLAGS_COMMON -O0 -g"
 
 gfortran $FFLAGS -c pao.f dns.f vfft.f visasub.f
-gfortran $FFLAGS -o dns_test.exe pao.o dns.o vfft.o visasub.o
-./dns_test.exe
+gfortran $FFLAGS -o fortran_test.exe pao.o dns.o vfft.o visasub.o
+./fortran_test.exe
 
